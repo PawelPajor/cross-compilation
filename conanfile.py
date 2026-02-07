3ad8e789af444b8c38446684a10e150e0e349c21
Pawel Pajor <pawel.pajor@gmail.com>
2026-02-07 16:53:46 +0000

    chore: simplify 'conanfile.py'

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class HelloGuiConan(ConanFile):
     name = "hello_gui"
     version = "0.1"
     settings = "os", "arch", "compiler", "build_type"
-    requires = ( "sdl/3.2.20" )
-    generators = ( "CMakeToolchain", "CMakeDeps" )
+    requires = "sdl/3.2.20"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def build(self):
         cmake = CMake(self)
